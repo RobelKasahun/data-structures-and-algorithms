5a4cb73532f3cc30267fcb77ef77425d7000e201
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-08-07 21:36:10 -0700

    Add a solution to find middle node problem

--- linked_list_ds/problems.py
+++ linked_list_ds/problems.py
@@ -9,26 +9,48 @@ for i in range(1, 11, 1):
 
 linked_list.print_list()
 
+# def find_middle_node(head):
+#     # slow and fast points to the head of the List
+#     slow = head
+#     fast = head
+        
+#     # empty list
+#     if not slow:
+#         return None
+    
+#     # iterate as long as there is a node next to the fast
+#     while fast.next:
+#         # move slow half of fast
+#         slow = slow.next
+            
+#         # move the fast two steps toward the end
+#         fast = fast.next
+#         if fast.next:
+#             fast = fast.next
+            
+#     # middle node of the List
+#     return slow.value
+
 def find_middle_node(head):
-    # slow and fast points to the head of the List
+    '''
+        find the middle node of a Linked List
+    '''
+    # initial pointers
     slow = head
     fast = head
-        
-    # empty list
+    
     if not slow:
         return None
+        
     
-    # iterate as long as there is a node next to the fast
     while fast.next:
-        # move slow half of fast
         slow = slow.next
-            
-        # move the fast two steps toward the end
         fast = fast.next
+        
         if fast.next:
             fast = fast.next
             
-    # middle node of the List
+    
     return slow.value
 
 print(find_middle_node(linked_list.get_head()))
