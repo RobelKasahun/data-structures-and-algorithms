2bd3a9c286017abb13561ec6d7eb3e54c2a71b93
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-09-01 20:57:44 -0700

    feat(linked_list): add get_head, get_tail, get_node, set_node, and pop methods

--- linked-list-dsa/final-linkedlist-implem/linkedlist.py
+++ linked-list-dsa/final-linkedlist-implem/linkedlist.py
@@ -12,10 +12,40 @@ class LinkedList(object):
         self.tail = None
         self.length = 0
         
+    def get_head(self):
+        '''
+        Returns the head or first node of the Linked List
+        
+        Args:
+            Does not take any arguments
+            
+        Returns:
+            node (head): Returns the head node of the Linked List
+            
+        Time Complexity:
+            O(1) - Constant time
+        '''
+        return self.head.value if self.head else 'None'
+    
+    def get_tail(self):
+        '''
+        Returns the end node or tail of the Linked List
+        
+        Args:
+            Does not take any arguments
+            
+        Returns:
+            node (tail): Returns the end node or tail of the Linked List
+            
+        Time Complexity:
+            O(1) - Constant time
+        '''
+        return self.tail.value if self.tail else 'None'
+        
     
     def append(self, value):
         '''
-        Adds a new node to the back or end of the Linked List.
+        Adds a new node with the given value to the back or end of the Linked List.
         
         Args:
             value (any): The value that will be added to the end of the Linked List.
@@ -45,6 +75,104 @@ class LinkedList(object):
             
         # a new node has been added successfully
         return True
+    
+    
+    def get_node(self, index):
+        '''
+        Return the node stored at the given index.
+        
+        Args:
+            index (int): An index where the node is stored.
+            
+        Returns:
+            node (any): The node stored at the given index in the Linked List.
+            
+        Time Complexity:
+            O(n) - pointer must be moved to the node stored at index.
+        '''
+        
+        # index out of range
+        if index < 0 or index >= self.length:
+            return None
+        
+        # pointer to the head of the Linked List
+        temp = self.head
+        
+        # iterate index times to reach the node at index
+        for _ in range(index):
+            temp = temp.next
+            
+        
+        return temp
+        
+    
+    def set_node(self, index, value):
+        '''
+        Set or update the value of a new positioned at the given index.
+        
+        Args:
+            index (int): An index where the node is stored.
+            value (any): A new value to be used to update the node at the given index in the Linked List.
+            
+        Returns:
+            node (any): The updated at the given index
+        '''
+        
+        # index out of range
+        if not self.get_node(index):
+            return None
+        
+        node = self.get_node(index)
+        node.value = value
+        
+        return node
+    
+    
+    def pop(self):
+        '''
+        Remove the tail or node at the end of the Linked List.
+        
+        Args:
+            This function does not take any arguments.
+            
+        Returns:
+            node: Returns the tail or removed node of the Linked List.
+            
+        Time Complexity:
+            O(n) - prev must be moved to a position before the tail node.
+                   temp must be moved to the tail of the Linked List.
+        '''
+        
+        temp = self.head
+        # empty Linked List
+        if not self.head and not self.tail:
+            return None
+        
+        # only one node in the Linked List
+        if self.length == 1:
+            self.head = self.tail = None
+            self.length -= 1
+            return temp
+        
+        # point to the head of the Linked List
+        temp = self.head
+        prev = None
+        
+        # Move the temp pointer through the Linked List nodes 
+        # to have previous and tail nodes
+        while temp.next:
+            prev = temp
+            temp = temp.next
+            
+        # make previous node the tail of the Linked List
+        self.tail = prev
+        # Its next pointer must point to None since it is the end of tail of the Linked List
+        self.tail.next = None
+        # decrease the Linked List node counts
+        self.length -= 1
+        # return the removed node
+        return temp
+            
         
     def print_list(self):
         '''
--- linked-list-dsa/final-linkedlist-implem/main.py
+++ linked-list-dsa/final-linkedlist-implem/main.py
@@ -6,18 +6,37 @@ print()
 
 # *** append(value) *** #
 linked_list = LinkedList()
-linked_list.append(1)
-linked_list.append(2)
-linked_list.append(3)
-linked_list.append(4)
-linked_list.append(5)
+append_result = linked_list.append(1)
+print(f'append_result: {append_result}')
+append_result = linked_list.append(2)
+print(f'append_result: {append_result}')
+append_result = linked_list.append(3)
+print(f'append_result: {append_result}')
+append_result = linked_list.append(4)
+print(f'append_result: {append_result}')
+append_result = linked_list.append(5)
+print(f'append_result: {append_result}')
+print()
 
-# *** length *** #
-print(linked_list.length)
+# *** pop() *** #
+pop_result = linked_list.pop()
+print(f'pop_result: {pop_result.value}')
+pop_result = linked_list.pop()
+print(f'pop_result: {pop_result.value}')
+pop_result = linked_list.pop()
+print(f'pop_result: {pop_result.value}')
+pop_result = linked_list.pop()
+print(f'pop_result: {pop_result.value}')
+pop_result = linked_list.pop()
+print(f'pop_result: {pop_result.value}')
+print()
 
 # head and tail
-print(f'head ----> {linked_list.head.value}')
-print(f'tail ----> {linked_list.tail.value}')
+print(f'head ----> {linked_list.get_head()}')
+print(f'tail ----> {linked_list.get_tail()}')
 
 # *** print_list *** #
 linked_list.print_list()
+
+# *** length *** #
+print(linked_list.length)
